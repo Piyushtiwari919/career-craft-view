8a2b1ddbaf99595288605e5ba144688dc827a790
Piyush Tiwari <piyushtiwari919921@gmail.com>
2025-09-07 10:42:57 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
         <div className="flex h-16 items-center justify-between">
           {/* Logo */}
           <div className="flex items-center space-x-2">
-            <div className="flex h-12 w-12 items-center justify-center">
+            <div className="flex h-15 w-15 items-center justify-center">
 {/*               <FileText className="h-6 w-6 text-white" /> */}
               <img src={logo} alt="SnapCV"/>
             </div>
