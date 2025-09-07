150600c52e44676e8d0036ecf4ccb66c0486d564
Piyush Tiwari <piyushtiwari919921@gmail.com>
2025-09-07 10:30:52 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Moon, Sun, FileText, Menu, X } from "lucide-react";
 import { useTheme } from "@/components/ThemeProvider";
+import logo from "src/components/SnapCV Logo.jpg";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -21,7 +22,7 @@ const Navbar = () => {
           <div className="flex items-center space-x-2">
             <div className="flex h-10 w-10 items-center justify-center rounded-lg gradient-brand">
 {/*               <FileText className="h-6 w-6 text-white" /> */}
-              <img src="src/components/SnapCV Logo.jpg"/>
+              <img src={logo}/>
             </div>
             <span className="text-xl font-bold text-foreground">SnapCV</span>
           </div>
