4dbab23a8dd27972819688b73c18a3de4ccc0fdd
Piyush Tiwari <piyushtiwari919921@gmail.com>
2025-09-07 10:29:04 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
           <div className="flex items-center space-x-2">
             <div className="flex h-10 w-10 items-center justify-center rounded-lg gradient-brand">
 {/*               <FileText className="h-6 w-6 text-white" /> */}
-              <img src="./SnapCV Logo.jpg"/>
+              <img src="src/components/SnapCV Logo.jpg"/>
             </div>
             <span className="text-xl font-bold text-foreground">SnapCV</span>
           </div>
